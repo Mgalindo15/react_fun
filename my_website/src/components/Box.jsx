ac77b68d2d359413589199829c8a9378d59ca86c
Matt Galindo <mgalindo15@pop-os.localdomain>
2024-10-15 19:24:09 -0500

    Added CSS eh

--- my_website/src/components/Box.jsx
+++ my_website/src/components/Box.jsx
@@ -14,9 +14,18 @@ const ShiftingBox = () => {
 
     return (
             <div
-            className="w-12 h-12 border border-black flex justify-center items-center font-noto text-lg absolute"
+            className="w-25 h-40 p-12 border border-black flex justify-center items-center 
+            font-noto font-bold text-lg absolute"
             onMouseEnter={handleHover} 
-            style={{ top: `${yPos}%`, left: `${xPos}%`, transform: 'translate(-50%, -50%)' }}
+            style={{ 
+                top: `${yPos}%`,
+                left: `${xPos}%`,
+                transform: 'translate(-50%, -50%)',
+                backgroundImage: 'linear-gradient(45deg, #4d4d4d, #899196, #e0e0e0)',
+                backgroundColor: '#899196',
+                borderRadius: '8px',
+                boxShadow: '1rem 1rem .8rem -.2rem rgba(0, 0, 0, 0.2)'
+            }}
             >
                 Catch Me
             </div>
